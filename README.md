ad1f1ce3c9baec502a5ea82c4edfb165f30f3296
Wang.Y <294961525@qq.com>
2023-10-20 21:47:43 +0800

    231020: add readme.md

---
+++ README.md
@@ -0,0 +1,20 @@
+# MailOC
+基于email，smtplib实现的发送邮件
+
+## 前置步骤
+在正式使用前需要将已实例化的mime和stp传入MailOC，如下：
+```python
+import smtplib
+from email.mime.multipart import MIMEMultipart
+
+# mime
+mime = MIMEMultipart('related')
+
+# stp
+stp = smtplib.SMTP()
+stp.connect("smtp.qq.com", 25)  # MAIL_HOST, MAIL_PORT 
+stp.login(os.environ.get("EMAIL"), os.environ.get("LICENSE"))   # EMAIL, LICENSE
+
+# init
+MailOC(mime, stp)
+```
--- mail_oc.py
+++ mail_oc.py
@@ -7,12 +7,12 @@ class MailOC:
     def __init__(self, mime, stp):
         self.mime = mime
         self.stp = stp
-        
+
     def get_mime_from(self, sender: Dict[str, str]):
         # sender: {"name": "xxx", "email": "xxx@xxx.com"}
 
         return f"{sender['name']}<{sender['email']}>"
-    
+
     def get_mime_to(self, receivers: List[Dict[str, str]]):
         # receivers: [{"name": "xxx1", "email": xxx@xxx.com}, {"name": "xxx2", "email": xxx@xxx.com}]
 
@@ -20,17 +20,21 @@ class MailOC:
         for receiver in receivers:
             mm_to += f"{receiver['name']}<{receiver['email']}>,"
         return mm_to
-    
+
     def set_from_to(self, sender: Dict[str, str], receivers: List[Dict[str, str]]):
         self.mime["From"] = self.get_mime_from(sender)
         self.mime["To"] = self.get_mime_to(receivers)
-    
+
     def set_subject(self, subject):
+        if self.mime["Subject"]:
+            del self.mime["Subject"]  # Create a new topic instead of appending
         self.mime["Subject"] = Header(subject, 'utf-8')
 
     def set_plain_body(self, body):
-        self.mime.attach(MIMEText(body,"plain","utf-8"))
-    
+        if self.mime.get_payload():
+            self.mime.set_payload([])
+        self.mime.attach(MIMEText(body, "plain", "utf-8"))
+
     def send_email(self, sender: str, receivers: List[str]):
         # sender: "xxx@xxx.com"
         # receivers: ["xxx@xxx.com", "xxx@xxx.com"]
